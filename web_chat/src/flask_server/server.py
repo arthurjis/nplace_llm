64b6766fa2c595fe136aa876cc43ce7b2880f256
arthurjis <jisheng@nplace.xyz>
2023-05-06 18:05:11 +0800

    add chatbot_base and simple_chatbot

--- web_chat/src/flask_server/server.py
+++ web_chat/src/flask_server/server.py
@@ -5,13 +5,12 @@ web_chat_dir = str(Path(__file__).resolve().parent.parent.parent)
 if web_chat_dir not in sys.path:
     sys.path.insert(0, web_chat_dir)
 
-
-
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 import logging
 from flask_cors import CORS
-from scripts import simple_chat
+from scripts.simple_chatbot import SimpleChatbot
+
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -19,13 +18,14 @@ CORS(app)  # Enable CORS for the Flask app
 
 # Configure logging settings for the Flask app
 app.logger.setLevel(logging.DEBUG)
+simple_chatbot = SimpleChatbot()
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.json['message']
     # app.logger.debug(f"Received user message: {user_message}")
 
-    responses = simple_chat.reply(user_message)
+    responses = simple_chatbot.reply(user_message)
     # app.logger.debug(f"Generated responses: {responses}")
 
     return jsonify(responses)
