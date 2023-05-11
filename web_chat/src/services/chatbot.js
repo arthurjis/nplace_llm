a0ef54a2ac66fb3497b1077ccca3e7e83bdc8a84
arthurjis <jisheng@nplace.xyz>
2023-05-11 22:14:04 +0800

    clean up

--- web_chat/src/services/chatbot.js
+++ web_chat/src/services/chatbot.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const SERVER_URL = 'http://127.0.0.1:5000/'; // Replace with your chatbot API endpoint
-// const SERVER_URL = "https://nplacellmflask-production.up.railway.app"
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export async function sendMessageToChatbot(message) {
