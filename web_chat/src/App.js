69964d808744b233ed6ad7ee73cba6ff411d9007
arthurjis <jisheng@nplace.xyz>
2023-05-13 16:55:27 +0800

    Update App.js

--- web_chat/src/App.js
+++ web_chat/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+import SocketContext from './contexts/SocketContext';
 import Chat from './components/Chat';
 import Login from './components/Login';
 import Registration from './components/Registration';
@@ -8,6 +10,7 @@ function App() {
 
   const [token, setToken] = useState(null);
   const [showRegistration, setShowRegistration] = useState(false);
+  const [socket, setSocket] = useState(null);
 
   const handleLogin = (token) => {
     setToken(token);
@@ -19,30 +22,43 @@ function App() {
     setToken(null);
   };
 
+  useEffect(() => {
+    const newSocket = socketIOClient('${SERVER_URL}');
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
   if (token) {
     return (
-      <div className="App">
-        <Chat />
-        <button onClick={handleLogout}>Logout</button>
-      </div>
+      <SocketContext.Provider value={socket}>
+        <div className="App">
+          <Chat />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </SocketContext.Provider>
     );
   }
 
   if (showRegistration) {
     return (
-      <div className="App">
-        <Registration onLogin={handleLogin} />
-        <button onClick={() => setShowRegistration(false)}>Back</button>
-      </div>
+      <SocketContext.Provider value={socket}>
+        <div className="App">
+          <Registration onLogin={handleLogin} />
+          <button onClick={() => setShowRegistration(false)}>Back</button>
+        </div>
+      </SocketContext.Provider>
     );
   }
 
   return (
-    <div className="App">
-      <Login onLogin={handleLogin} />
-      <button onClick={() => setShowRegistration(true)}>Register</button>
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className="App">
+        <Login onLogin={handleLogin} />
+        <button onClick={() => setShowRegistration(true)}>Register</button>
+      </div>
+    </SocketContext.Provider>
   );
+
 }
 
 export default App;
