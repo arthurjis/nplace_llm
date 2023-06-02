6c9db110c7a68144d24fe7f8bd9933e7bebd7cd9
arthurjis <jisheng@nplace.xyz>
2023-06-02 13:44:32 +0800

    Update Signup.js

--- web_chat/src/components/Signup.js
+++ web_chat/src/components/Signup.js
@@ -1,99 +1,114 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, TextField, Typography, Grid, Link, InputAdornment, IconButton } from '@material-ui/core';
+import { Button, TextField, Typography, Grid, Link, InputAdornment, IconButton, Box } from '@material-ui/core';
 import EditIcon from '@mui/icons-material/Edit';
 import { Link as RouterLink } from 'react-router-dom';
 
 function Signup({ onLogin }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [step, setStep] = useState(1);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [step, setStep] = useState(1);
 
-  const handleContinue = async (event) => {
-    event.preventDefault();
-    if (step === 1) {
-      setStep(2);
-    } else {
-      const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-      try {
-        await axios.post(`${SERVER_URL}/register`, { email, password });
-        const loginResponse = await axios.post(`${SERVER_URL}/login`, { email, password });
-        onLogin(loginResponse.data.access_token);
+    const handleContinue = async (event) => {
+        event.preventDefault();
+        if (step === 1) {
+            setStep(2);
+        } else {
+            const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+            try {
+                await axios.post(`${SERVER_URL}/register`, { email, password });
+                const loginResponse = await axios.post(`${SERVER_URL}/login`, { email, password });
+                onLogin(loginResponse.data.access_token);
 
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
 
-  return (
-    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-      <div style={{ width: '300px' }}>
-        <form onSubmit={handleContinue}>
-          <Typography variant="h5" component="h2" gutterBottom style={{ fontWeight: 'bold' }}>
-            Create your account
-          </Typography>
-          {step === 1 ? (
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Email Address"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          ) : (
-            <>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                label="Email Address"
-                value={email}
-                disabled
-                InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={() => setStep(1)} style={{ backgroundColor: 'transparent' }}>
-                          <EditIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </>
-          )}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={{ height: "52px", borderRadius: 2 }}
-          >
-            Continue
-          </Button>
-          <Typography variant="body2">
-            Already have an account?{' '}
-            <Link component={RouterLink} to="/login" variant="body2" color="secondary" style={{ textTransform: 'none', backgroundColor: 'transparent', textDecoration: 'none' }}>
-              Log In
-            </Link>
-          </Typography>
-        </form>
-      </div>
-    </Grid>
-  );
+    return (
+        <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
+            <div style={{ width: '320px' }}>
+                <form onSubmit={handleContinue}>
+                    <Box pt={2}>
+                        <Typography variant="h5" component="h2" gutterBottom style={{ fontWeight: 'bold', fontSize: '32px' }}>
+                            Create your account
+                        </Typography>
+                    </Box>
+                    {step === 1 ? (
+                        <>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Email Address"
+                                autoFocus
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                InputProps={{
+                                    style: { height: "52px", borderRadius: 2 }
+                                }}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                fullWidth
+                                label="Email Address"
+                                value={email}
+                                disabled
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={() => setStep(1)} style={{ backgroundColor: 'transparent', position: 'relative', right: '-10px' }}>
+                                                <EditIcon />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                    style: { height: "52px", borderRadius: 2 }
+                                }}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="none"
+                                required
+                                fullWidth
+                                label="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                InputProps={{
+                                    style: { height: "52px", borderRadius: 2 }
+                                }}
+                            />
+                        </>
+                    )}
+                    <Box pt={2}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            style={{ height: "52px", borderRadius: 2, textTransform: 'none', fontSize: '16px' }}
+                        >
+                            Continue
+                        </Button>
+                    </Box>
+                    <Box pt={2}>
+                        <Typography variant="body2">
+                            Already have an account?{' '}
+                            <Link component={RouterLink} to="/login" variant="body2" color="secondary" style={{ textTransform: 'none', backgroundColor: 'transparent', textDecoration: 'none' }}>
+                                Log In
+                            </Link>
+                        </Typography>
+                    </Box>
+                </form>
+            </div>
+        </Grid>
+    );
 }
 
 export default Signup;
