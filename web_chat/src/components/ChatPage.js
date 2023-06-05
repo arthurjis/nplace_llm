b7211309aea83888d51e8a856de521b37ef572b1
arthurjis <jisheng@nplace.xyz>
2023-06-05 16:26:36 +0800

    Update ChatPage.js

--- web_chat/src/components/ChatPage.js
+++ web_chat/src/components/ChatPage.js
@@ -1,23 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import socketIOClient from "socket.io-client";
-import Chat from './Chat'; // Please adjust the import paths according to your project structure
-import ChatSessionList from './ChatSessionList'; // Same here
+import Chat from './Chat';
+import SidePanel from './SidePanel';
 import SocketContext from '../contexts/SocketContext';
+import { Box, Drawer, Hidden } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 
 
 const ChatPage = ({ token, onLogout }) => {
     const [socket, setSocket] = useState(null);
     const [selectedChatSession, setSelectedChatSession] = useState(null);
     const [refreshChatSessionsSignal, setRefreshChatSessionsSignal] = useState(Date.now());
-
     const navigate = useNavigate();
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    useEffect(() => {
-        if (!token) {
-            navigate("/login");
-        }
-    }, [token, navigate]);
+
+
+    const handleDrawerToggle = () => {
+        console.log("setMobileOpen " + !mobileOpen);
+        setMobileOpen(!mobileOpen);
+
+    };
+
+
+    // useEffect(() => {
+    //     if (!token) {
+    //         navigate("/login");
+    //     }
+    // }, [token, navigate]);
 
     const handleLogout = () => {
         if (socket) {
@@ -28,9 +46,9 @@ const ChatPage = ({ token, onLogout }) => {
         onLogout();
     };
 
-    const handleStartChat = () => {
-        setSelectedChatSession(null);
-    };
+    // const handleStartChat = () => {
+    //     setSelectedChatSession(null);
+    // };
 
     const handleChatSessionSelect = (chatSession) => {
         setSelectedChatSession(chatSession);
@@ -59,38 +77,188 @@ const ChatPage = ({ token, onLogout }) => {
         }
     }, [socket]);
 
+
+
     return (
-        <SocketContext.Provider value={socket}>
-            <div className="ChatPage">
-                {socket ? (
-                    <>
-                        <div className="flex-container">
-                            <ChatSessionList
-                                token={token}
-                                onChatSessionSelect={handleChatSessionSelect}
-                                refreshChatSessionsSignal={refreshChatSessionsSignal}
-                            />
-                            <div>
-                                <Chat
-                                    token={token}
-                                    selectedChatSession={selectedChatSession}
-                                    setSelectedChatSession={setSelectedChatSession}
-                                    refreshChatSessions={handleRefreshChatSessions}
-                                />
-                                <button onClick={handleStartChat}>Start New Chat Session</button>
-                                <button onClick={handleLogout}>Logout</button>
-                            </div>
-                        </div>
-                    </>
-                ) : (
-                    <p>Connecting...</p>
-                )}
-            </div>
-        </SocketContext.Provider>
+        <>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: '100vw',
+                    height: '98vh',
+                    position: 'absolute',
+                    // overflowX: 'auto',
+                }}
+            >
+                <Box // SidePanel Parent Container
+                    sx={{
+                        position: 'absolute',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        // height: '78.4%',
+                        height: '80%',
+                        width: '300px',
+                        backgroundColor: 'transparent',
+                        paddingRight: mobileOpen ? '730px' : '400px',
+                        transition: 'padding 0.5s ease',
+                        zIndex: 1,
+                        border: 1,
+                        borderColor: '#AA1231',
+                        borderStyle: 'dashed',
+                    }}
+                >
+                    <Box // SidePanel
+                        sx={{
+                            backgroundColor: 'gray',
+                            height: '100%',
+                            width: '100%',
+                        }}
+                    >
+
+                    </Box>
+                </Box>
+
+                <Box // ChatPanel Parent Container
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '80%',
+                        width: '100%',
+                        maxWidth: '700px',
+                        minWidth: '700px',
+                        backgroundColor: 'transparent',
+                        paddingLeft: mobileOpen ? '330px' : '0px',
+                        transition: 'padding 0.5s ease',
+                        zIndex: 2,
+                            border: 2,
+                            borderColor: '#AA1231',
+                            borderRadius: '30px',
+                            borderStyle: 'solid',
+                    }}
+                >
+                    <Box // ChatPanel
+                        sx={{
+                            backgroundColor: 'yellow',
+                            height: '100%',
+                            width: '100%',
+                        }}
+                    >
+
+                    </Box>
+                </Box>
 
 
 
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="end"
+                    onClick={handleDrawerToggle}
+                    sx={{
+                        position: 'fixed',
+                        bottom: 20,
+                        right: 20,
+                        bgcolor: 'white',
+                        borderRadius: '50%',
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+            </Box>
+        </>
     );
 };
 
 export default ChatPage;
+
+
+
+        // <SocketContext.Provider value={socket}>
+        //     <Box
+        //         sx={{
+        //             display: 'flex',
+        //             flexDirection: 'row',
+        //             alignItems: 'center',
+        //             justifyContent: 'center',
+        //             height: '100vh',
+        //             backgroundColor: '#FFFFAA',
+        //         }}
+        //     >
+        //         {socket ? (
+        //             <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'center' }}>
+        //                 <Hidden smDown implementation="css">
+        //                     <Box
+        //                         sx={{
+        //                             bgcolor: 'white',
+        //                             border: 2,
+        //                             borderColor: 'primary.main',
+        //                             borderRadius: '12px',
+        //                             borderStyle: 'solid',
+        //                             p: 2,
+        //                             height: '80vh',
+        //                             overflowY: 'scroll',
+        //                             width: 300,
+        //                         }}
+        //                     >
+        //                         {/* <SidePanel
+        //                                 token={token}
+        //                                 onChatSessionSelect={handleChatSessionSelect}
+        //                                 refreshChatSessionsSignal={refreshChatSessionsSignal}
+        //                                 onLogout={handleLogout}
+        //                             /> */}
+        //                     </Box>
+        //                 </Hidden>
+
+        //                 <Box
+        //                     sx={{
+        //                         bgcolor: 'white',
+        //                         border: 2,
+        //                         borderColor: '#AA1231',
+        //                         borderRadius: '12px',
+        //                         borderStyle: 'solid',
+        //                         p: 2,
+        //                         height: '80vh',
+        //                         overflowY: 'scroll',
+        //                         // flexGrow: 1,
+        //                         // maxWidth: 900,
+        //                         minWidth: 600,
+        //                     }}
+        //                 >
+        //                     {/* <Chat
+        //                             token={token}
+        //                             selectedChatSession={selectedChatSession}
+        //                             setSelectedChatSession={setSelectedChatSession}
+        //                             refreshChatSessions={handleRefreshChatSessions}
+        //                         /> */}
+        //                 </Box>
+
+        //                 <Hidden mdUp implementation="js">
+        //                     <Drawer
+        //                         variant="temporary"
+        //                         open={mobileOpen}
+        //                         onClose={handleDrawerToggle}
+        //                         ModalProps={{
+        //                             keepMounted: true, // Better open performance on mobile.
+        //                         }}
+        //                     >
+        //                         <SidePanel
+        //                             token={token}
+        //                             onChatSessionSelect={handleChatSessionSelect}
+        //                             refreshChatSessionsSignal={refreshChatSessionsSignal}
+        //                             onLogout={handleLogout}
+        //                         />
+        //                     </Drawer>
+        //                 </Hidden>
+        //             </Box>
+        //         ) : (
+        //             <p>Connecting...</p>
+        //         )}
+        //     </Box>
+
+        // </SocketContext.Provider>
