e0b2fe51f07d324c9407a31d7937f9544c433349
arthurjis <jisheng@nplace.xyz>
2023-06-07 17:30:54 +0800

    Update ThemeUtils.js

--- web_chat/src/utils/ThemeUtils.js
+++ web_chat/src/utils/ThemeUtils.js
@@ -3,12 +3,28 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#6200EE',
+      // main: '#6200EE',
+      // main: '#B1B695',
+      main: '#b9dcf2',
+      light: 'red',
+      dark: '#0085d8',
+      contrastText: '#596175',
     },
     secondary: {
-      main: '#6200EE',
+      main: '#f6cfbe',
+      light: '#f6cfbe',
+      dark: '#f6cfbe',
+      contrastText: '#f6cfbe',
     },
-    background: '#f5f5f5'
+    background: '#f5f5f5',
+    liked: {
+      main: 'red',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#888888',
+    },
+
   },
 });
 
