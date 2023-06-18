5b27daf990bb8d39808a28f90d942b9d154edada
arthurjis <jisheng@nplace.xyz>
2023-06-18 15:19:19 +0800

    Update ChatSessionList.js

--- web_chat/src/components/ChatSessionList.js
+++ web_chat/src/components/ChatSessionList.js
@@ -4,6 +4,8 @@ import ListItemButton from '@mui/material/ListItemButton';
 import Box from '@mui/material/Box';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 
 
@@ -50,63 +52,108 @@ function ChatSessionList({ token, onChatSessionSelect, refreshChatSessionsSignal
     onChatSessionSelect(chatSession);
   };
 
-  useEffect(() => {
-    fetchChatSessions();
-  }, [token, refreshChatSessionsSignal, fetchChatSessions]);
+  const deleteChatSession = (chatSessionId, event) => {
+    console.log(`Delete chat session ${chatSessionId}`);
+    if (!token) {
+      return;
+    }
+    event.stopPropagation();  // Stop event from bubbling up to the ListItemButton's onClick handler
+    fetch(process.env.REACT_APP_SERVER_URL + '/chat_sessions/' + chatSessionId, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          if (response.status === 401) {
+            // TODO: Handle 401 status (bad token)
+            handleLogout();
+            throw new Error("Unauthorized");
+          } else if (response.status === 403) {
+            // TODO: Handle 403 status (forbidden)
+            throw new Error("You are not authorized to view this chat session.");
+          } else if (response.status === 404) {
+            // TODO: Handle 404 status (not found)
+            throw new Error("The chat session you're trying to access was not found.");
+          }
+          throw new Error("Network response was not ok");
+        }
+        console.log(`Deleted chat session ${chatSessionId}`);
+        fetchChatSessions();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
-  return (
-    <Box sx={{ width: '100%', bgcolor: 'background.sidePanel' }}>
-      <List component="nav" aria-label="chat sessions">
-        {chatSessions.map(chatSession => (
-          <ListItemButton
-            key={chatSession.id}
-            onClick={() => onSelectSession(chatSession.id)}
-            sx={{
-              height: '64px',
-              left: '1%',
-              width: "98%",
-              marginBottom: '12px',
-              alignSelf: 'flex-start',
-              borderRadius: '15px',
-              boxShadow: 3,
-              backgroundColor: chatSession.id === selectedChatSessionID ? 'primary.light' : 'transparent',
-              '&.MuiListItemButton-root': {
-                '&:hover': {
-                  backgroundColor: 'primary.light', // override hover state
-                },
-                '&.Mui-focusVisible': {
-                  backgroundColor: 'primary.light', // override focus state
-                },
-                '& .MuiTouchRipple-root': {
-                  display: 'none', // disable ripple effect
-                },
+
+useEffect(() => {
+  fetchChatSessions();
+}, [token, refreshChatSessionsSignal, fetchChatSessions]);
+
+return (
+  <Box sx={{ width: '100%', bgcolor: 'background.sidePanel' }}>
+    <List component="nav" aria-label="chat sessions">
+      {chatSessions.map(chatSession => (
+        <ListItemButton
+          key={chatSession.id}
+          onClick={() => onSelectSession(chatSession.id)}
+          sx={{
+            height: '64px',
+            left: '1%',
+            width: "98%",
+            marginBottom: '12px',
+            alignSelf: 'flex-start',
+            borderRadius: '15px',
+            boxShadow: 3,
+            backgroundColor: chatSession.id === selectedChatSessionID ? 'primary.light' : 'transparent',
+            '&.MuiListItemButton-root': {
+              '&:hover': {
+                backgroundColor: 'primary.light', // override hover state
               },
-              '&.Mui-selected': {
-                backgroundColor: 'primary.light', // override selected state
+              '&.Mui-focusVisible': {
+                backgroundColor: 'primary.light', // override focus state
               },
-            }}
-          >
-            <ListItemIcon>
-              <ChatBubbleOutlineIcon
-                sx={{
-                  fontSize: '20px',
-                  color: 'primary.main.contrastText',
-                }}
-              />
-            </ListItemIcon>
-            <ListItemText
-              primary={chatSession.name}
-              primaryTypographyProps={{ 
-                fontSize: '1em', 
-                color: 'text.primary',
-                fontWeight: 500 
-              }} 
+              '& .MuiTouchRipple-root': {
+                display: 'none', // disable ripple effect
+              },
+            },
+            '&.Mui-selected': {
+              backgroundColor: 'primary.light', // override selected state
+            },
+          }}
+        >
+          <ListItemIcon>
+            <ChatBubbleOutlineIcon
+              sx={{
+                fontSize: '20px',
+                color: 'primary.main.contrastText',
+              }}
             />
-          </ListItemButton>
-        ))}
-      </List>
-    </Box>
-  );
+          </ListItemIcon>
+          <ListItemText
+            primary={chatSession.name}
+            primaryTypographyProps={{
+              fontSize: '1em',
+              color: 'text.primary',
+              fontWeight: 500
+            }}
+          />
+          {chatSession.id === selectedChatSessionID &&
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={(event) => deleteChatSession(chatSession.id, event)}
+            >
+              <DeleteOutlineIcon sx={{ fontSize: '24px', color: 'primary.main.contrastText' }} />
+            </IconButton>
+          }
+        </ListItemButton>
+      ))}
+    </List>
+  </Box>
+);
 }
 
 export default ChatSessionList;
